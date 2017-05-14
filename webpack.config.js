9c2c088a15b1aa7278f30deac21269424a2b1554
mojtaba <zarei.bs@gmail.com>
2017-05-14 11:36:58 +0430

    #1 using this library in browser

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: "./jalali-moment.js", // string | object | array
+    output: {
+        path: path.resolve(__dirname, "dist"), // string
+        filename: "jalali-moment.js", // string
+        // the url to the output directory resolved relative to the HTML page
+        library: "jalali-moment", // string,
+        // the name of the exported library
+        libraryTarget: "umd", // universal module definition
+        // the type of the exported library
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
+}
