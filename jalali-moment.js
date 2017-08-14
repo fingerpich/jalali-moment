738f1ca3f0094898147ff731cafb0a7f16f39452
mojtaba <zarei.bs@gmail.com>
2017-08-14 13:45:16 +0430

    add formatPersian to jalali moment

--- jalali-moment.js
+++ jalali-moment.js
@@ -906,11 +906,19 @@ jMoment.fn.clone = function () {
     return jMoment(this);
 };
 
-jMoment.fn.loadPersian = function (usePersianDigits) {
-    jMoment.loadPersian(usePersianDigits);
-    var persianClone = jMoment(this);
-    jMoment.unloadPersian();
-    return persianClone;
+jMoment.fn.formatPersian = function (format) {
+    var prevLocale = this.locale();
+    var globalState = moment.usePersianDigits;
+
+    this.locale("fa");
+    moment.usePersianDigits = true;
+
+    this.format(format);
+    var formated = this.format(format);
+
+    moment.usePersianDigits = globalState;
+    this.locale(prevLocale);
+    return formated;
 };
 
 jMoment.useJalaliSystemPrimarily = function (calendarSystem) {
@@ -954,59 +962,63 @@ jMoment.unloadPersian = function () {
     moment.updateLocale(moment.prevLocale);
 };
 
+jMoment.defineFaLocale = function(){
+    moment.defineLocale("fa", {
+        months: ("ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر").split("_")
+        , monthsShort: ("ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر").split("_")
+        , weekdays: ("یک\u200cشنبه_دوشنبه_سه\u200cشنبه_چهارشنبه_پنج\u200cشنبه_آدینه_شنبه").split("_")
+        , weekdaysShort: ("یک\u200cشنبه_دوشنبه_سه\u200cشنبه_چهارشنبه_پنج\u200cشنبه_آدینه_شنبه").split("_")
+        , weekdaysMin: "ی_د_س_چ_پ_آ_ش".split("_")
+        , longDateFormat:
+            { LT: "HH:mm"
+                , L: "jYYYY/jMM/jDD"
+                , LL: "jD jMMMM jYYYY"
+                , LLL: "jD jMMMM jYYYY LT"
+                , LLLL: "dddd، jD jMMMM jYYYY LT"
+            }
+        , calendar:
+            { sameDay: "[امروز ساعت] LT"
+                , nextDay: "[فردا ساعت] LT"
+                , nextWeek: "dddd [ساعت] LT"
+                , lastDay: "[دیروز ساعت] LT"
+                , lastWeek: "dddd [ی پیش ساعت] LT"
+                , sameElse: "L"
+            }
+        , relativeTime:
+            { future: "در %s"
+                , past: "%s پیش"
+                , s: "چند ثانیه"
+                , m: "1 دقیقه"
+                , mm: "%d دقیقه"
+                , h: "1 ساعت"
+                , hh: "%d ساعت"
+                , d: "1 روز"
+                , dd: "%d روز"
+                , M: "1 ماه"
+                , MM: "%d ماه"
+                , y: "1 سال"
+                , yy: "%d سال"
+            }
+        , ordinal: "%dم"
+        , week:
+            { dow: 6 // Saturday is the first day of the week.
+                , doy: 12 // The week that contains Jan 1st is the first week of the year.
+            }
+        , meridiem: function (hour) {
+            return hour < 12 ? "ق.ظ" : "ب.ظ";
+        }
+        , jMonths: ("فروردین_اردیبهشت_خرداد_تیر_مرداد_شهریور_مهر_آبان_آذر_دی_بهمن_اسفند").split("_")
+        , jMonthsShort: "فرو_ارد_خرد_تیر_مرد_شهر_مهر_آبا_آذر_دی_بهم_اسف".split("_")
+    });
+}
+jMoment.defineFaLocale();
+moment.locale("en");
+
 jMoment.loadPersian = function (usePersianDigits) {
     if (moment.locale()!=="fa") {
         moment.prevLocale = moment.locale();
     }
     moment.usePersianDigits = usePersianDigits;
-    moment.locale("fa", {
-            months: ("ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر").split("_")
-            , monthsShort: ("ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر").split("_")
-            , weekdays: ("یک\u200cشنبه_دوشنبه_سه\u200cشنبه_چهارشنبه_پنج\u200cشنبه_آدینه_شنبه").split("_")
-            , weekdaysShort: ("یک\u200cشنبه_دوشنبه_سه\u200cشنبه_چهارشنبه_پنج\u200cشنبه_آدینه_شنبه").split("_")
-            , weekdaysMin: "ی_د_س_چ_پ_آ_ش".split("_")
-            , longDateFormat:
-                { LT: "HH:mm"
-                    , L: "jYYYY/jMM/jDD"
-                    , LL: "jD jMMMM jYYYY"
-                    , LLL: "jD jMMMM jYYYY LT"
-                    , LLLL: "dddd، jD jMMMM jYYYY LT"
-                }
-            , calendar:
-                { sameDay: "[امروز ساعت] LT"
-                    , nextDay: "[فردا ساعت] LT"
-                    , nextWeek: "dddd [ساعت] LT"
-                    , lastDay: "[دیروز ساعت] LT"
-                    , lastWeek: "dddd [ی پیش ساعت] LT"
-                    , sameElse: "L"
-                }
-            , relativeTime:
-                { future: "در %s"
-                    , past: "%s پیش"
-                    , s: "چند ثانیه"
-                    , m: "1 دقیقه"
-                    , mm: "%d دقیقه"
-                    , h: "1 ساعت"
-                    , hh: "%d ساعت"
-                    , d: "1 روز"
-                    , dd: "%d روز"
-                    , M: "1 ماه"
-                    , MM: "%d ماه"
-                    , y: "1 سال"
-                    , yy: "%d سال"
-                }
-            , ordinal: "%dم"
-            , week:
-                { dow: 6 // Saturday is the first day of the week.
-                    , doy: 12 // The week that contains Jan 1st is the first week of the year.
-                }
-            , meridiem: function (hour) {
-                return hour < 12 ? "ق.ظ" : "ب.ظ";
-            }
-            , jMonths: ("فروردین_اردیبهشت_خرداد_تیر_مرداد_شهریور_مهر_آبان_آذر_دی_بهمن_اسفند").split("_")
-            , jMonthsShort: "فرو_ارد_خرد_تیر_مرد_شهر_مهر_آبا_آذر_دی_بهم_اسف".split("_")
-        }
-    );
 };
 
 jMoment.jConvert =  { toJalali: toJalali
