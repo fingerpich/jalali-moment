7f7127931060e56c5bb6e98f03a2c515ba517c61
Pouya MN <pouya.malekinejad@gmail.com>
2019-06-29 00:34:14 +0430

    fixes bug in parsing minutes instead of months
    fixes bug #63

--- jalali-moment.js
+++ jalali-moment.js
@@ -171,13 +171,13 @@ function normalizeUnits(units, momentObj) {
     if (isJalali(momentObj)) {
         units = toJalaliUnit(units);
     }
-    if (units) {
+     if (units) {
         var lowered = units.toLowerCase();
-        units = unitAliases[lowered] || lowered;
+        if (lowered.startsWith('j')) units = unitAliases[lowered] || lowered;
+        // TODO : add unit test
+        if (units === "jday") units = "day";
+        else if (units === "jd") units = "d";
     }
-    // TODO : add unit test
-    if (units === "jday") units = "day";
-    else if (units === "jd") units = "d";
     return units;
 }
 
